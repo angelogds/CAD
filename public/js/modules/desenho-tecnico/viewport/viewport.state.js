a8e949b2c3df933ea89bdbfa22b857bb428901ce
angelogds <126365784+angelogds@users.noreply.github.com>
2026-03-17 13:50:09 -0300

    Melhora UX e corrige coordenadas/viewport do editor CAD

--- public/js/modules/desenho-tecnico/viewport/viewport.state.js
+++ public/js/modules/desenho-tecnico/viewport/viewport.state.js
@@ -1,1 +1,9 @@
-export const createViewportState = () => ({ zoom: 1, minZoom: 0.1, maxZoom: 30, centerX: 0, centerY: 0, offsetX: 0, offsetY: 0, width: 1, height: 1 });
+export const createViewportState = () => ({
+  zoom: 1,
+  minZoom: 0.1,
+  maxZoom: 30,
+  offsetX: 0,
+  offsetY: 0,
+  width: 1,
+  height: 1,
+});
