5247e9a9d766280184f8e2c01572e18eb1b2ddf8
angelogds <126365784+angelogds@users.noreply.github.com>
2026-09-13 20:18:30 -0300

    fix(rh): restaurar rotas de colaboradores e meu portal

--- server.js
+++ server.js
@@ -391,6 +391,8 @@ mount(OFFICIAL_ROUTES.estoque, "./modules/estoque/estoque.routes");
 mount(OFFICIAL_ROUTES.almoxarifado, "./modules/almoxarifado/almoxarifado.routes");
 mount("/escala", "./modules/escala/escala.routes");
 mount(OFFICIAL_ROUTES.rh, "./modules/rh/rh.routes");
+mount("/colaboradores", "./modules/colaboradores/colaboradores.routes");
+mount("/meu-portal", "./modules/meu-portal/meu-portal.routes");
 mount("/avisos", "./modules/avisos/avisos.routes");
 mount("/usuarios", "./modules/usuarios/usuarios.routes");
 mount("/admin", "./modules/admin/storage.routes");
