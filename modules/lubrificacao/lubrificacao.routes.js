21d9e8a44f282d4664b892ed8b942aa338c9f0c0
angelogds <126365784+angelogds@users.noreply.github.com>
2026-09-21 11:59:56 -0300

    feat(lubrificacao-v6): atualizar lubrificacao.routes.js

--- modules/lubrificacao/lubrificacao.routes.js
+++ modules/lubrificacao/lubrificacao.routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const EXECUTION_ACCESS = ACCESS.lubrificacao_execucao;
 
 router.get('/', requireLogin, requireRole(EXECUTION_ACCESS), ctrl.index);
+router.get('/relatorio-semanal.pdf', requireLogin, requireRole(EXECUTION_ACCESS), ctrl.relatorioSemanalPdf);
 router.post('/:id/executar', requireLogin, requireRole(EXECUTION_ACCESS), ctrl.executar);
 
 module.exports = router;
